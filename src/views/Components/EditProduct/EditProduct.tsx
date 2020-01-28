daa651b7b61dc8c13c9a424fee0554e55527388e
Artem Filippov <f-artyom@yandex.ru>
2020-01-28 08:48:36 +0300

    EditProduct: поправил нативную валидацию; добавил подсказки; в поле наличие автоматически выставлено значение 0;

--- src/views/Components/EditProduct/EditProduct.tsx
+++ src/views/Components/EditProduct/EditProduct.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import './EditProduct.scss';
 import {IProduct} from "../../../types/Product";
 import Button from "../../Components/Button/Button";
@@ -14,14 +14,24 @@ interface IEditProductPageProps {
 }
 
 const EditProductPage: React.FC<IEditProductPageProps> = ({saveProductHandler, error, product, changeCodeHandler}) => {
+    const formRef = useRef<HTMLFormElement | null>(null);
     const [code, setCode] = useState(product == null ? '' : product.code);
     const [shortName, setShortName] = useState(product == null ? '' : product.shortName);
     const [fullName, setFullName] = useState(product == null ? '' : product.fullName);
-    const [availabilityValue, setAvailabilityValue] = useState(product == null ? '' : product.availability.value);
+    const [availabilityValue, setAvailabilityValue] = useState(product == null ? '0' : product.availability.value);
     const [availabilityUnit, setAvailabilityUnit] = useState(product == null ? UNITS[0] : product.availability.unit);
     const [imageUrl, setImageUrl] = useState(product == null ? '' : product.image_url);
 
     const onSendForm = (e: any) => {
+        if (formRef.current == null) {
+            return;
+        }
+
+        const formIsValid = formRef.current.reportValidity();
+        if (formIsValid === false) {
+            return false;
+        }
+
         e.preventDefault();
         saveProductHandler({
             code,
@@ -41,48 +51,40 @@ const EditProductPage: React.FC<IEditProductPageProps> = ({saveProductHandler, e
     const onFullNameChange = (e: any) => setFullName(e.target.value);
     const onAvailabilityValueChange = (e: any) => setAvailabilityValue(e.target.value);
     const onAvailabilityUnitChange = (e: any) => {
-        setAvailabilityUnit(e.target.value);
+        setAvailabilityUnit(e.target.value)
     };
     const onImageUrlChange = (e: any) => setImageUrl(e.target.value);
-
-    // TODO: добавить валидацию
-    const canSaveProduct = code.trim().length > 0
-        && shortName.trim().length > 0
-        && fullName.trim().length > 0
-        && availabilityValue.toString().trim().length > 0
-        && availabilityUnit.trim().length > 0
-        && imageUrl.trim().length > 0;
 
     return (
         <div className="edit-product-component">
             {error != null && <div className="error">{error}</div>}
-            <form className="edit-product-component__form" onSubmit={onSendForm}>
+            <form className="edit-product-component__form" onSubmit={onSendForm} ref={formRef}>
                 <div className="edit-product-component__row">
                     <label className="edit-product-component__label">Код товара:</label>
-                    <input className="edit-product-component__input" required={true} placeholder="P-0000000" type="text" value={code} onChange={onCodeChange}/>
+                    <input className="edit-product-component__input" required={true} placeholder="Введите код в формате P-0000000" type="text" value={code} onChange={onCodeChange}/>
                 </div>
                 <div className="edit-product-component__row">
                     <label className="edit-product-component__label">Краткое название:</label>
-                    <input className="edit-product-component__input" required={true} minLength={5} maxLength={60} type="text" value={shortName} onChange={onShortNameChange}/>
+                    <input className="edit-product-component__input" required={true} placeholder="Введите краткое название товара от 2 до 60 символов" minLength={2} maxLength={60} type="text" value={shortName} onChange={onShortNameChange}/>
                 </div>
 
                 <div className="edit-product-component__row">
                     <label className="edit-product-component__label">Полное название:</label>
-                    <input className="edit-product-component__input" type="text" minLength={5} value={fullName} onChange={onFullNameChange}/>
+                    <input className="edit-product-component__input" required={true} placeholder="Введите полное название товара от 2 до 100 символов" type="text" minLength={2} maxLength={100} value={fullName} onChange={onFullNameChange}/>
                 </div>
                 <div className="edit-product-component__row">
                     <label className="edit-product-component__label">Наличие товара:</label>
-                    <input className="edit-product-component__input" type="number" min={0} value={availabilityValue} onChange={onAvailabilityValueChange}/>
-                    <select className="edit-product-component__input" value={availabilityUnit} onChange={onAvailabilityUnitChange}>
+                    <input className="edit-product-component__input" required={true} type="number" min={0} placeholder="Введите число остатка" value={availabilityValue} onChange={onAvailabilityValueChange}/>
+                    <select className="edit-product-component__input" required={true} value={availabilityUnit} onChange={onAvailabilityUnitChange}>
                         {UNITS.map((unit) => <option key={unit} value={unit}>{unit}</option>)}
                     </select>
                 </div>
                 <div className="edit-product-component__row">
                     <label className="edit-product-component__label">Изображение:</label>
-                    <input className="edit-product-component__input" placeholder="https://lenta.gcdn.co/globalassets/1/-/35/28/72/238094.png" type="text" value={imageUrl} onChange={onImageUrlChange}/>
+                    <input className="edit-product-component__input" required={true} placeholder="https://lenta.gcdn.co/globalassets/1/-/35/28/72/238094.png" type="text" value={imageUrl} onChange={onImageUrlChange}/>
                 </div>
                 <div>
-                    <Button text="Сохранить" clickHandler={onSendForm} isDisabled={!canSaveProduct}/>
+                    <Button text="Сохранить" clickHandler={onSendForm}/>
                     <Link to="/" className="btn btn_error">Отмена</Link>
                 </div>
             </form>
